d0e60d1ab297e4cbae420077a779c6d88f9b8653
Naiba <1@5.nu>
2019-01-16 10:42:44 +0800

    Remove login id

--- login.go
+++ login.go
@@ -2,18 +2,16 @@ package ucenter
 
 import (
 	"time"
-
-	"github.com/jinzhu/gorm"
 )
 
 // Login 登录的终端
 type Login struct {
-	gorm.Model
-	UserID uint
-	Name   string
-	IP     string
-	Token  string
-	Expire time.Time
+	UserID    uint
+	Name      string
+	IP        string
+	Token     string `gorm:"primary_key"`
+	Expire    time.Time
+	CreatedAt time.Time
 
 	User User
 }
